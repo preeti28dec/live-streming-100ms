c3ac36d439363d7c7ceaf7c07fe8297e869dc2e7
preeti28dec <preetithakur28dec2000@gmail.com>
2022-12-22 11:35:08 +0530

    add css

--- components/ChatNDParticipants.jsx
+++ components/ChatNDParticipants.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import Message from "./Message";
+import InputEmoji from "react-input-emoji";
 import {
   selectHMSMessages,
   useHMSActions,
   useHMSStore,
 } from "@100mslive/react-sdk";
 import { selectPeers } from "@100mslive/react-sdk";
-import { useState } from "react";
-import InputEmoji from "react-input-emoji";
+import { IoMdChatboxes } from "react-icons/io";
+import {FaUsers} from 'react-icons/fa'
 
 function ChatNdParticipants() {
   const [selectedOption, setSelectedOption] = useState("chat");
@@ -15,7 +17,14 @@ function ChatNdParticipants() {
   const hmsActions = useHMSActions();
   const peers = useHMSStore(selectPeers);
 
+  const [text, setText] = useState("");
+
+  function handleOnEnter(text) {
+    console.log("enter", text);
+  }
+
   const handleSubmit = (e) => {
+    // e.preventDefault();
     hmsActions.sendBroadcastMessage(message);
     setMessage("");
   };
@@ -25,43 +34,46 @@ function ChatNdParticipants() {
       <div className="rightBox__head">
         <span
           onClick={() => setSelectedOption("chat")}
-          className={selectedOption === "chat" ? "selected" : ""}
-        >
-          Chat
+          className={selectedOption === "chat" ? "selected" : ""}>
+          Chat <IoMdChatboxes/>
         </span>
         <span
           className={selectedOption === "participants" ? "selected" : ""}
           onClick={() => setSelectedOption("participants")}
         >
-          Participants
+          Participants <FaUsers/>
         </span>
       </div>
+
       <div className="rightBox__optionView">
         {selectedOption === "chat" && (
           <>
-            <div className="rightBox__chats scroll-y">
+            <div className="rightBox__chats">
               {/* Messages */}
               {messages.map((msg) => (
                 <Message key={msg.id} message={msg} />
               ))}
             </div>
-            <InputEmoji
-              value={message}
-              onChange={setMessage}
-              cleanOnEnter
-              onEnter={handleSubmit}
-              placeholder="Type a message"
-            />
+
+            <div className="inputEmoji">
+              <InputEmoji
+                value={message}
+                onChange={setMessage}
+                cleanOnEnter
+                onEnter={handleSubmit}
+                placeholder="Type a message"
+              />
+            </div>
           </>
         )}
         {selectedOption === "participants" && (
           <div className="rightBox__participants">
             {/* Participants */}
             {peers.map((peer) => (
-              <div className="rightBox__participant" key={peer.id}>
-                {peer.name}
-                <p>{peer.roleName}</p>
-              </div>
+              <>
+                <span className="rightBox__participant_role">{peer.roleName}</span>
+                <div className="rightBox__participant">{peer.name}</div>
+              </>
             ))}
           </div>
         )}
